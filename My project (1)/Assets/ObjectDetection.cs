9a84c97241901aff8d58a675504e2811d9a4071d
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-04-05 12:08:26 +0200

    added item cumlector

---
+++ My project (1)/Assets/ObjectDetection.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetection : MonoBehaviour
+{
+    public List<GameObject> droppedItemsList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        droppedItemsList.Add(other.gameObject);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.name);
+        
+        other.gameObject.SetActive(false);
+    }
+}
