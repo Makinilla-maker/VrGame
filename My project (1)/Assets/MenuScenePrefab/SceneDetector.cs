08d6e57e713ab828ad06c8411d17ccbcb2ec1bfa
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-04-09 19:02:45 +0200

    asd

---
+++ My project (1)/Assets/MenuScenePrefab/SceneDetector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Assertions;
+using UnityEngine.XR.Provider;
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+public class SceneDetector : MonoBehaviour
+{
+    public TeleportationProvider a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
