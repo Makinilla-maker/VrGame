4e2b3cfa6328bf404a43d64f604cc2e49dfb0d9e
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-04-12 12:06:26 +0200

    aaaa

---
+++ My project (1)/Assets/FBX/TiraChinas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiraChinas : MonoBehaviour
+{
+    public GameObject anchor1;
+    public GameObject anchor2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<SpringJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
