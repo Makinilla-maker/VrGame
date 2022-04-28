e98ab1a5a301fb3842e0bdfe71c5adeedcc882ff
Makinilla-maker <valverdeoriol@gmail.com>
2022-04-29 00:37:33 +0200

    tengo mucho sueño, buenas noches

---
+++ My project (1)/Assets/Scripts/Events.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Events
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f,1f)]
+    public float volume;
+    [Range(.0f,3f)]
+    public float pitch;
+
+    public bool loop;
+    public bool outManager;
+    public GameObject prebab;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
