27aeff7ee910879f96f8971c08d704fc78cfe6b7
Makinilla-maker <valverdeoriol@gmail.com>
2022-04-30 00:18:42 +0200

    tete muchos cambios

--- My project (1)/Assets/ObjectDetection.cs
+++ My project (1)/Assets/ObjectDetection.cs
@@ -18,11 +18,20 @@ public class ObjectDetection : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.gameObject.name);
         if(other.gameObject.tag != "Controller")    droppedItemsList.Add(other.gameObject);
+        if(other.gameObject.tag == "Tape")  TimeForGo(1);
+     
     }
     private void OnTriggerStay(Collider other)
     {
         Debug.Log(other.name);
         if(other.gameObject.tag != "Controller")    other.gameObject.SetActive(false);
+    }
+    private void TimeForGo(int id)
+    {
+        //effecto de camera VHS
+        //tp a escena
+        GameObject.Find("XRRig").gameObject.transform.position = new Vector3(-22.5799999f,-0.430000007f,10.5200005f);
     }
 }
