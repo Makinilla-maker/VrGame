c045e940d1883d4054bad05c14c38633ecbe23da
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-05-01 13:43:00 +0200

    asd

---
+++ My project (1)/Assets/KeyPad/HandButton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HandButton : MonoBehaviour
+{
+    [SerializeField] private float threshold = 0.1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    public Key generalPassword;
+    private bool isPressed;
+    private Color startColor;
+    private Vector3 startPos;
+    public UnityEvent onPressed, onReleased;
+    public GameObject selectedButton;
+    [SerializeField] private ConfigurableJoint joint;
+
+    void Start()
+    {
+        startColor = GetComponent<Renderer>().material.color;
+        joint = GetComponent<ConfigurableJoint>();
+        startPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        // if(!isPressed && GetValue() + threshold >= 1)
+        //     Pressed();
+
+        // if(isPressed && GetValue() - threshold <= 0)
+        //     Released();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        isPressed = true;
+        if(generalPassword.pas[generalPassword.i] == '0' && isPressed)
+        {
+            generalPassword.pas[generalPassword.i] = this.gameObject.name[0];
+        }
+        generalPassword.s = new string(generalPassword.pas);
+        Debug.Log("Pressed");
+    }
+    void OnTriggerExit(Collider other)
+    {
+        generalPassword.i++;
+        isPressed = false;
+        Debug.Log("Released");
+    }
+}
--- My project (1)/Assets/KeyPad/Key.cs
+++ My project (1)/Assets/KeyPad/Key.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor.UIElements;
 public class Key : MonoBehaviour
 {
     public bool keyHit = false;
+
+    public TextMeshProUGUI textDisplay;
     private Color originalColor;
     private Renderer renderer;
+    public int i = 0;
+
+    public char[] pas = new char[] {'0','0','0','0'};
+    public char[] correctPas = new char[] {'2','8','4','5'};
     private float colorReturnTime = 0.1f;
     private float returnColor;
+    public string s;
     // Start is called before the first frame update
     void Start()
     {
+        s = new string(pas);
         renderer = GetComponent<Renderer>();
         originalColor = renderer.material.color;
     }
@@ -19,6 +29,7 @@ public class Key : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        textDisplay.text = s;
         if(keyHit&&returnColor<Time.time)
         {
             returnColor = Time.time+colorReturnTime;
