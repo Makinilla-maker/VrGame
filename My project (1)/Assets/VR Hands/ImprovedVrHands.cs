32aec9ef8698e9f811cac5f7d7f2c33dceadaa42
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-05-02 13:57:13 +0200

    hands

---
+++ My project (1)/Assets/VR Hands/ImprovedVrHands.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ImprovedVrHands : MonoBehaviour
+{
+    [SerializeField] InputActionReference controllerActionGrip;
+    [SerializeField] InputActionReference controllerActionTrigger;
+    private Animator handAnimator;
+
+    private void Awake()
+    {
+        controllerActionGrip.action.performed += GripPress;
+        controllerActionTrigger.action.performed += TriggerPress;
+
+        handAnimator = GetComponent<Animator>();
+    }
+
+    private void TriggerPress(InputAction.CallbackContext obj) => handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+
+    private void GripPress(InputAction.CallbackContext obj) => handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+}
