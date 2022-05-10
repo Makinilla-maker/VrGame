81e97ba5910aab78871ab63ada1fdc083d7d43f3
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-05-10 20:26:41 +0200

    a

--- My project (1)/Assets/Scripts/Performance.cs
+++ My project (1)/Assets/Scripts/Performance.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Performance : MonoBehaviour
 {
     public bool loadAsylum;
+    public bool unLoadFirstPuzzle;
     private bool firstRoomLoadOneTime;
     public GameObject puzzleFirstRoom;
+    public GameObject wallToBlockFirstPuzzle;
     // Start is called before the first frame update
     void Start()
     {
         puzzleFirstRoom.gameObject.SetActive(false);
         firstRoomLoadOneTime = false;
+        wallToBlockFirstPuzzle.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,6 +24,10 @@ public class Performance : MonoBehaviour
         {
             puzzleFirstRoom.gameObject.SetActive(true);
             firstRoomLoadOneTime = true;
+        }
+        if(unLoadFirstPuzzle)
+        {
+            wallToBlockFirstPuzzle.SetActive(true);
         }
     }
 }
---
+++ My project (1)/Assets/Scripts/TriggerCode.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCode : MonoBehaviour
+{
+    [SerializeField] private string name;
+    public Performance performance;
+    private bool unLoadFirstPuzzle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = gameObject.name;
+        unLoadFirstPuzzle = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(name == "CubeLocateSecondPuzzle")
+        {
+            unLoadFirstPuzzle=true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Controller" && unLoadFirstPuzzle)
+        {
+            Debug.Log("Enter to colliders");
+            performance.unLoadFirstPuzzle = true;
+            unLoadFirstPuzzle = false;
+        }
+    }
+}
